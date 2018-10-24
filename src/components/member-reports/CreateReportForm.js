3a63ea5877f421d2b1ebc9f1c2c3ccb69f72c54c
Rain Tomista <rstomista@up.edu.ph>
2018-10-24 22:33:27 +0800

    Added confirmation to create report

--- src/components/member-reports/CreateReportForm.js
+++ src/components/member-reports/CreateReportForm.js
@@ -56,49 +56,59 @@ export default class CreateReportForm extends MobxReactForm {
   hooks() {
     return {
       async onSuccess(form) {
-        const { reportStore, sessionStore } = RootStore;
-        const { loggedUser } = sessionStore; 
- 
-        form.$('receiver_id').set('value', loggedUser.barangay_page_id);
-
-        // Disable fields on submit
-        form.$('report_type').set('disabled', true);
-        form.$('committee_type').set('disabled', true);        
-        form.$('message').set('disabled', true);
-
-        let {
-          message,
-          receiver_id,
-          report_type,
-          committee_type,
-        } = form.values();
-        committee_type = report_type === 'General' ? null : committee_type.toLowerCase();
-        report_type = report_type.toLowerCase();
-        let files = [];
-
-        try {
-          // Set form data
-          let formData = new FormData();
-          formData.append('message', message);
-          formData.append('receiver_id', receiver_id);
-          formData.append('report_type', report_type);
-          formData.append('committee_type', committee_type);
-          formData.append('files', '');
-
-          await createReport(formData); //Http request for creating new 
-          this.clear(); // Clear form when the request is successful
-          NavigationService.dispatch(StackActions.pop()); //Go back to the previous screen
-          ToastAndroid.show(localized.CREATE_REPORT_SUCCESS, ToastAndroid.SHORT);
-          reportStore.refreshMyReports();
-        } catch(e) {
-          // Enable fields on error
-          form.$('report_type').set('disabled', false);
-          form.$('committee_type').set('disabled', false);        
-          form.$('message').set('disabled', false);
-
-          // Display error message
-          ToastAndroid.show(localized.REQUEST_ERROR, ToastAndroid.SHORT);
-        }
+        Alert.alert(
+          'Create Report',
+          'Are you sure you want to create this report?',
+          [
+            {text: 'Cancel', style: 'cancel'},
+            {text: 'OK', onPress: async () => {
+              const { reportStore, sessionStore } = RootStore;
+              const { loggedUser } = sessionStore; 
+       
+              form.$('receiver_id').set('value', loggedUser.barangay_page_id);
+      
+              // Disable fields on submit
+              form.$('report_type').set('disabled', true);
+              form.$('committee_type').set('disabled', true);        
+              form.$('message').set('disabled', true);
+      
+              let {
+                message,
+                receiver_id,
+                report_type,
+                committee_type,
+              } = form.values();
+              committee_type = report_type === 'General' ? null : committee_type.toLowerCase();
+              report_type = report_type.toLowerCase();
+              let files = [];
+      
+              try {
+                // Set form data
+                let formData = new FormData();
+                formData.append('message', message);
+                formData.append('receiver_id', receiver_id);
+                formData.append('report_type', report_type);
+                formData.append('committee_type', committee_type);
+                formData.append('files', '');
+      
+                await createReport(formData); //Http request for creating new 
+                this.clear(); // Clear form when the request is successful
+                NavigationService.dispatch(StackActions.pop()); //Go back to the previous screen
+                ToastAndroid.show(localized.CREATE_REPORT_SUCCESS, ToastAndroid.SHORT);
+                reportStore.refreshMyReports();
+              } catch(e) {
+                // Enable fields on error
+                form.$('report_type').set('disabled', false);
+                form.$('committee_type').set('disabled', false);        
+                form.$('message').set('disabled', false);
+      
+                // Display error message
+                ToastAndroid.show(localized.REQUEST_ERROR, ToastAndroid.SHORT);
+              }
+            }},
+          ],
+          { cancelable: false }
+        );
       },
       onError(form) {
         ToastAndroid.show(localized.FIELDS_REQUIRED, ToastAndroid.SHORT);

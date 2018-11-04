9715c7eacb3308b520cd834f405edb69f43c726c
Rain Tomista <rstomista@up.edu.ph>
2018-11-04 19:03:35 +0800

    Restyle member sidebar

--- src/screens/member/MemberSidebar.js
+++ src/screens/member/MemberSidebar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { observer } from 'mobx-react';
-import { AppRegistry, AsyncStorage, Image, StatusBar } from "react-native";
-import { Container, Content, Text, List, ListItem } from "native-base";
+import {  
+  AsyncStorage,  
+  ScrollView,
+  StatusBar, 
+  StyleSheet, 
+  TouchableNativeFeedback,
+  View 
+} from "react-native";
+import { Container, Content, Text, List, ListItem, Thumbnail} from "native-base";
+import MemberAvatar from '../../../assets/images/default-member.png';
 import { getUserDetails } from '../../services/AuthService';
 import RootStore from 'stores/RootStore';
+import * as colors from 'styles/colors';
+import * as fonts from 'styles/fonts';
+
 
 const routes = [
   {key: 'Home', label: 'Home'},
   {key: 'Search', label: 'Search'},
   {key: 'MyBarangay', label: 'My Barangay'},  
   {key: 'Messages', label: 'Messages'},  
-  {key: 'MyProfile', label: 'Profile'},    
   {key: 'MyFollowing', label: 'Following'},  
   {key: 'MyReportStack', label: 'Reports'}, 
   {key: 'MyRespondedReportStack', label: 'Responded'},    
@@ -18,25 +28,66 @@ const routes = [
 
 @observer
 export default class MemberSidebar extends React.Component {
+  async componentWillMount() {
+    await RootStore.sessionStore.getLoggedUser();
+  }
   render() {
+    const { loggedUser } = RootStore.sessionStore;
+    
     return (
       <Container>
-        <Content>
+        <ScrollView>
+          {loggedUser && (
+            <TouchableNativeFeedback
+            onPress={() => {
+              this.handlePress({key: 'MyProfile'})
+            }}
+            >
+              <View style={styles.loggedMemberContainer}>
+                <Thumbnail 
+                  circle 
+                  source={MemberAvatar} 
+                  style={styles.loggedMemberAvatar}
+                />
+                <Text style={styles.loggedMemberName} numberOfLines={1}>
+                  {`${loggedUser.user_first_name} ${loggedUser.user_last_name}`}
+                </Text>
+                <Text style={styles.loggedMemberLocation}>
+                  {loggedUser.barangay_page_name},
+                </Text>
+                <Text style={styles.loggedMemberLocation}>
+                  {loggedUser.barangay_page_municipality}
+                </Text>
+              </View>
+            </TouchableNativeFeedback>
+          )}
           <List
             dataArray={routes}
+            style={{marginVertical: 12}}
             renderRow={(route) => {
               return (
                 <ListItem button
                   onPress={() => {
                     this.handlePress(route)
                   }}
+                  style={{borderBottomWidth: 0, paddingTop: 12, paddingBottom: 12 }}
                 >
-                  <Text>{route.label}</Text>
+                  <Text 
+                    style={{
+                      color: colors.PRIMARY, 
+                      fontFamily: fonts.LATO_MEDIUM, 
+                      fontSize: 17.5,
+                      fontWeight: 'normal'
+                    }}
+                  >
+                    {route.label}
+                  </Text>
                 </ListItem>
               );
             }}
           />
-        </Content>
+        </ScrollView>
+
       </Container>
     );
   }
@@ -49,3 +100,34 @@ export default class MemberSidebar extends React.Component {
     this.props.navigation.navigate(route.key);                    
   }
 }
+
+
+const styles = StyleSheet.create({
+  loggedMemberContainer: {
+    backgroundColor: colors.PRIMARY,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 25,
+    paddingHorizontal: 15
+  },
+  loggedMemberAvatar: {
+    borderRadius: 150,
+    height: 150,
+    width: 150,
+    marginBottom: 10
+  },
+  loggedMemberName: {
+    alignSelf: 'center',
+    color: colors.LIGHT,
+    fontFamily: fonts.MONTSERRAT_BOLD,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  loggedMemberLocation: {
+    alignSelf: 'center',
+    color: colors.LIGHT,
+    fontFamily: fonts.LATO_REGULAR,
+    fontSize: 22,
+    textAlign: 'center'
+  }
+});

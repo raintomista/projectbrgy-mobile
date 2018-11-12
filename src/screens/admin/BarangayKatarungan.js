f8c9c722f92b44b8b7c7c7aaf65b252f3cafc1de
Rain Tomista <rstomista@up.edu.ph>
2018-11-13 01:30:05 +0800

    Implement infinite scroll in katarungang pambarangay

--- src/screens/admin/BarangayKatarungan.js
+++ src/screens/admin/BarangayKatarungan.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import { observer } from 'mobx-react';
 import { action, observable, runInAction } from 'mobx';
-import { AsyncStorage, Dimensions, StyleSheet, ScrollView, TouchableOpacity, ToastAndroid, View } from 'react-native';
+import { AsyncStorage, FlatList, Dimensions, StyleSheet, ScrollView, TouchableOpacity, ToastAndroid, View } from 'react-native';
 import { Container, Content, Spinner, Text } from 'native-base';
 import { Table, TableWrapper, Row } from 'react-native-table-component';
 import { HeaderWithDrawer } from 'components/common';
 import { getAllComplaints } from 'services/EService';
+import NavigationService from 'services/NavigationService';
 import * as localized from 'localization/en';
 import * as colors from 'styles/colors';
 import * as fonts from 'styles/fonts.js'
@@ -16,19 +17,36 @@ export default class BarangayKatarungan extends Component {
   @observable page = 0;
   @observable limit = 20;
   @observable order = 'desc';
+  @observable hasMore = true;
+
   @observable tableHeader = ['Complainant', ' Offender Name', 'Allegation/s', 'Complaint Date'];
   @observable tableWidth = [200, 200, 220, 220];
-  @observable tableData = [];
+  @observable tableData = null;
 
   @action
   async getComplaints(brgyId) {
     this.page += 1;
     try {
-      const response = await getAllComplaints(brgyId, this.page, this.limit, this.order);
+      const response = await getAllComplaints(brgyId, this.page, this.limit, this.order);   
       if(response.data.data.katarungang_pambarangay.length > 0) {
-        runInAction(() => this.tableData.push(...response.data.data.katarungang_pambarangay));
+        runInAction(() => {
+          if(!this.tableData) {
+            this.tableData = response.data.data.katarungang_pambarangay;
+          } else {
+            this.tableData.push(...response.data.data.katarungang_pambarangay);
+          }
+        });
+      } else {
+        runInAction(() => {
+          this.hasMore = false;
+          this.error = true;
+        });
       }
     } catch(e) {
+      runInAction(() => {
+        this.hasMore = false;
+        this.error = true;
+      });
       ToastAndroid.show(localized.REQUEST_ERROR, ToastAndroid.SHORT);
     }
   }
@@ -51,51 +69,90 @@ export default class BarangayKatarungan extends Component {
     );
   }
 
+  renderItem = ({item, index}) => {
+    const rowData = [];
+    rowData.push(this.renderCell(item.name_of_complainant, item.status));
+    rowData.push(this.renderCell(item.name_of_offender, item.status));
+    rowData.push(this.renderCell(item.allegations, item.status));
+    rowData.push(this.renderCell(this.formatDate(item.date_created), item.status));
+
+    return (
+      <TouchableOpacity>
+        <Row 
+          key={index}
+          data={rowData} 
+          widthArr={Array.from(this.tableWidth)}
+          borderStyle={{borderWidth: 0}}
+          style={[item.status === 'unread' ? styles.tableDataUnreadRow : styles.tableDataRow, index === 0 && {borderTopWidth: 0}]}
+        />  
+      </TouchableOpacity>
+    );
+  }
+
+  renderLoader(hasMore) {
+    if(hasMore === false) return null;
+    return (
+      <Spinner 
+        color={colors.PRIMARY} 
+        style={{ backgroundColor: colors.LIGHT }} 
+      />
+    );
+  }
+
+  renderList(tableData, hasMore, error) {
+    return (
+      <FlatList
+        data={Array.from(tableData)}
+        renderItem={(props) => this.renderItem(props)}
+        keyExtractor={(item) => item.id}
+        onEndReached={() => this.handleLoadMore(error)}
+        onEndReachedThreshold={1}
+        initialNumToRender={10}
+        maxToRenderPerBatch={10}    
+        ListFooterComponent={() => this.renderLoader(hasMore)} 
+      />
+    );
+  }
+
   render() {
     return (
       <Container>
         <HeaderWithDrawer title="Katarungang Pambarangay Complaints" />
         <View style={styles.view}>
-          <ScrollView horizontal={true}>
-            <View>
-              <Table borderStyle={{borderWidth: 0}} style={styles.tableHeader}>
-                <Row 
-                  data={Array.from(this.tableHeader)} 
-                  widthArr={Array.from(this.tableWidth)}  
-                  style={styles.tableHeaderRow}
-                  textStyle={styles.tableHeaderText}
-                />
-              </Table>
-                <ScrollView>
-                  <Table  borderStyle={{borderWidth: 0}}>
+          {!this.tableData && (
+            <Spinner color={colors.PRIMARY} />
+          )}
+          {this.tableData && (
+            <ScrollView horizontal={true} style={{ flexGrow: 0 }}>
+              <View>
+                <Table borderStyle={{borderWidth: 0}} style={styles.tableHeader}>
+                  <Row 
+                    data={Array.from(this.tableHeader)} 
+                    widthArr={Array.from(this.tableWidth)}  
+                    style={styles.tableHeaderRow}
+                    textStyle={styles.tableHeaderText}
+                  />
+                </Table>
+                <ScrollView style={{marginLeft: -1, marginBottom: -1}}>
+                  <Table>
                     <TableWrapper>
-                      {this.tableData.map((data, index) => {
-                        const rowData = [];
-                        rowData.push(this.renderCell(data.name_of_complainant, data.status));
-                        rowData.push(this.renderCell(data.name_of_offender, data.status));
-                        rowData.push(this.renderCell(data.allegations, data.status));
-                        rowData.push(this.renderCell(this.formatDate(data.date_created), data.status));
-
-                        return (
-                          <TouchableOpacity>
-                            <Row 
-                              key={index}
-                              data={rowData} 
-                              widthArr={Array.from(this.tableWidth)}
-                              borderStyle={{borderWidth: 0}}
-                              style={[data.status === 'unread' ? styles.tableDataUnreadRow : styles.tableDataRow, index === 0 && {borderTopWidth: 0}]}
-                            />  
-                          </TouchableOpacity>
-                        );
-                      })}
+                      {this.renderList(this.tableData, this.hasMore, this.error)}
                     </TableWrapper>
                   </Table>
                 </ScrollView>
-            </View>
-          </ScrollView>
+              </View>
+            </ScrollView>
+          )}       
         </View>
       </Container>
     );
+  }
+
+  async handleLoadMore(error) {
+    const brgyId = await AsyncStorage.getItem('brgy-id');
+    if(!error) {
+      this.getComplaints(brgyId);
+    }
   }
 
   formatDate(date) {
@@ -128,7 +185,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.LIGHT,
     borderTopWidth: 1,
     borderTopColor: '#d2d2d2',
-    paddingLeft: 20,
+    paddingLeft: 23,
     paddingRight: 20,
     width: null
   },
@@ -141,7 +198,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#dee2e6',
     borderTopWidth: 1,
     borderTopColor: '#d2d2d2',
-    paddingLeft: 20,
+    paddingLeft: 23,
     paddingRight: 20,
     width: null
   },

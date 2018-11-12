988232a93d8fd499be9795535cfe7fd79afda0ec
Rain Tomista <rstomista@up.edu.ph>
2018-11-13 03:33:00 +0800

    Implement barangay clearance screen

--- src/services/EService.js
+++ src/services/EService.js
@@ -3,6 +3,15 @@ import { AsyncStorage } from 'react-native';
 import API_HOST from '../config';
 
 
+export async function getAllClearances(brgyId, page, limit) {
+    const token = await AsyncStorage.getItem('x-access-token');
+    return axios.get(`${API_HOST}/barangay-clearance/requests/admin/${brgyId}?page=${page}&limit=${limit}&order=desc`, {
+        headers: {
+            'x-access-token': token
+        }
+    });
+}
+
 export async function getAllPermits(brgyId, page, limit) {
     const token = await AsyncStorage.getItem('x-access-token');
     return axios.get(`${API_HOST}/business-permit/requests/admin/${brgyId}?page=${page}&limit=${limit}&order=desc`, {
